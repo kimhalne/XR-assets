8e32233d6b57ab871ad0addd56e586fe0280cc72
kimhalne <kimhalne69@gmail.com>
2019-05-21 11:22:07 +0900

    Gvrコントローラのボタンで、トリガーとサンプル画像がリセットされるスクリプトを追加

---
+++ Assets/Scripts/ResetTriggerObj.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetTriggerObj : MonoBehaviour
+{
+
+	[SerializeField]
+	GameObject TriggerObj;
+
+	[SerializeField]
+	GameObject TargetObj;
+	
+	[SerializeField]
+	float ActiveTime = 0.1f;
+	
+	//public KeyCode _switchKey;
+
+	private void OtherObjActive ()
+	{
+		//ターゲットオブジェクトの表示処理
+		TriggerObj.SetActive (true);
+		TargetObj.SetActive (false);
+	}
+
+    void Update(){
+
+        if (GvrControllerInput.ClickButtonDown)
+		//if (Input.GetKeyDown(_switchKey))	
+
+			//指定秒数後に、トリガーを表示しオブジェクトを非表示にする
+			Invoke ("OtherObjActive", ActiveTime);
+
+	}
+}
